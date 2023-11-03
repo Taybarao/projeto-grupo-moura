eaa1352ac705cbd8ad2bf98ea4edf095ae43dd6c
Tay Bar;ão <130027098+Taybarao@users.noreply.github.com>
2023-11-02 22:34:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const botaoMenu = document.querySelector('.botao-menu')
 const menu = document.querySelector('.menu-lateral')
+const pagina = document.querySelector('.conteudo-pagina')
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral-ativo')
+    pagina.classList.toggle('desfoque-pagina')
 })
+
+const botaoServiço = document.querySelector('.botao-serviços')
+const listaServiços = document.querySelector('.container-serviços')
+const btServiço = document.querySelector('.btn-serviço')
+
+
+botaoServiço.addEventListener('click', () => {
+    listaServiços.classList.toggle('container-serviços-ativo')
+    btServiço.classList.toggle('btn-serviço-ativo')
+    pagina.classList.toggle('desfoque-pagina')
+})
+
+
+
+
+
+
+
+
 
 window.sr = ScrollReveal({reset: true });
 sr.reveal('.titulo-conteudo-principal', {duration: 2000 });
