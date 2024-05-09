85e3041689fc79a86cf1ab18b53b41feae9c0f05
Tay Bar;ão <130027098+Taybarao@users.noreply.github.com>
2024-05-08 21:48:39 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ window.addEventListener("load",start)
   sr.reveal('.titulo-apresentaçao', {duration: 4000 });
    sr.reveal('.texto-apresentaçao', {duration: 8000 });
     sr.reveal('.logo-empresa', {duration: 6000 });
-
+    sr.reveal('.beneficios', {duration: 6000 });
 //botao aba menu lateral
 const botaoMenu = document.querySelector('.botao-menu')
  const menu = document.querySelector('.menu-lateral')
@@ -46,16 +46,16 @@ botaoServiço.addEventListener('click', () => {
 })
 
 
-const sliderPrincipal = document.querySelector('.s1');
+const sliderPrincipal = document.querySelector('.m1');
 const btSlideEsquerdo = document.querySelector('.bt-esquerdo');
 const btSlideDireito = document.querySelector('.bt-direito');
 
 btSlideDireito.addEventListener('click', () => {
-  sliderPrincipal.classList.remove('s1-desativado');
-  sliderPrincipal.classList.add('s1-ativo');
+  sliderPrincipal.classList.remove('m1-desativado');
+  sliderPrincipal.classList.add('m1-ativo');
 });
 
 btSlideEsquerdo.addEventListener('click', () => {
-  sliderPrincipal.classList.remove('s1-ativo');
-  sliderPrincipal.classList.add('s1-desativado');
+  sliderPrincipal.classList.remove('m1-ativo');
+  sliderPrincipal.classList.add('m1-desativado');
 });
